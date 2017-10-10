ea7d83e554b2aad0765471f13b424e2b3c01d3b6
coltenarave <coltenarave@comcast.net>
2017-10-10 13:02:07 -0600

    Updated template

--- Nitrogen_HW4_Task1.cpp
+++ Nitrogen_HW4_Task1.cpp
@@ -1,6 +1,16 @@
+// File: blank
+// Created by Nitrogen on 10/10/17 for CS1410.
+// Copyright (c) 2017 WSU
+//
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+// Constants, Structs, Classes
+
+// Prototypes
+
+// Main Program Program
+int main(void) {
     return 0;
 }
+// Function Definitions
