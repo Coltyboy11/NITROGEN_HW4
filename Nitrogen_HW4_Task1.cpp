863fd55323025b54bd3bea9215146f95b825aef8
coltenarave <coltenarave@comcast.net>
2017-10-11 17:30:52 -0600

    Changes to methods

--- Nitrogen_HW4_Task1.cpp
+++ Nitrogen_HW4_Task1.cpp
@@ -6,38 +6,51 @@
 
 using namespace std;
 // Constants, Structs, Classes
-class HouseInfo{
+class HouseInfo
+{
 private:
     int streetNumber;
     double housePrice;
     string streetName;
 public:
-     //Defaults when creating a new object
-    HouseInfo():streetNumber(0),housePrice(0),streetName(""){};
-    void getInfo();
-    void printInfo();
+    //Defaults when creating a new object
+
+
+    void getInfo(HouseInfo h[])
+    {
+        for (int i = 0; i < 3; i++)
+        {
+
+            cout << "Please enter info for house " << i+1 << endl;
+            cout << "Enter the street number: " << endl;
+            cin >> h[i].streetNumber;
+            cout << "Enter the street name: " << endl;
+            cin >> h[i].streetName;
+            cout << "Enter the price of the house: " << endl;
+            cin >> h[i].housePrice;
+        }
+
+    }
+    void printInfo(HouseInfo h[])
+    {
+        cout << "Your market analysis is as follows: " << endl;
+        for(int i = 0; i < 3; i++)
+        {
+            cout << i+1 << " house at: " << h[i].streetNumber << " " << h[i].streetName << " for $" << h[i].housePrice << endl;
+        }
+    }
+
 };
-void HouseInfo ::getInfo() {
-    cout<<"Type the street Number"<<endl;
-    cin>>streetNumber;
-    cout<<"What is the house price"<<endl;
-    cin>>housePrice;
-    cout<<"What is the street name";
-    cin>>streetName;
-}
-void HouseInfo ::printInfo() {
-    cout<<"The house on "<<streetNumber<<" "<<streetName<<" costs $"<<housePrice<<endl;
-}
 // Prototypes
 
 // Main Program Program
 int main(void) {
 
-    for (int i = 0; i < 2; i++) {
-      HouseInfo h[i];
-      h[i].getInfo();
-      h[i].printInfo();
-    }
+    HouseInfo h1;
+    HouseInfo h[3];
+    h1.getInfo(h);
+    h1.printInfo(h);
+
     return 0;
 }
 // Function Definitions
