89f88b525ecbe720d92a40126486b6f8fbe86bda
TrevorNewey <31452349+TrevorNewey@users.noreply.github.com>
2017-10-11 11:25:00 -0600

    Added Class HouseInfo with variables streetNumber,housePrice,streetName. Added 2 methods getinfo and printinfo.

--- Nitrogen_HW4_Task1.cpp
+++ Nitrogen_HW4_Task1.cpp
@@ -6,11 +6,38 @@
 
 using namespace std;
 // Constants, Structs, Classes
-
+class HouseInfo{
+private:
+    int streetNumber;
+    double housePrice;
+    string streetName;
+public:
+     //Defaults when creating a new object
+    HouseInfo():streetNumber(0),housePrice(0),streetName(""){};
+    void getInfo();
+    void printInfo();
+};
+void HouseInfo ::getInfo() {
+    cout<<"Type the street Number"<<endl;
+    cin>>streetNumber;
+    cout<<"What is the house price"<<endl;
+    cin>>housePrice;
+    cout<<"What is the street name";
+    cin>>streetName;
+}
+void HouseInfo ::printInfo() {
+    cout<<"The house on "<<streetNumber<<" "<<streetName<<" costs $"<<housePrice<<endl;
+}
 // Prototypes
 
 // Main Program Program
 int main(void) {
+
+    for (int i = 0; i < 2; i++) {
+      HouseInfo h[i];
+      h[i].getInfo();
+      h[i].printInfo();
+    }
     return 0;
 }
 // Function Definitions
